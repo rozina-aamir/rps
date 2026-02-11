4ea58473dffa0d5eb3857b6b8116e20125d3a8d0
rozina-aamir <rozina.aamir@outlook.com>
2026-02-11 22:12:02 +0000

    Edit parameters of getComputerChoice function

--- rps.js
+++ rps.js
@@ -8,7 +8,7 @@ console.log("Hello World");
 //
 // Log the result to the console
 
-function getComputerChoice(computerChoice) {
+function getComputerChoice(rock, paper, scissors) {
     return computerChoice;
 }
 
