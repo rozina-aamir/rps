f9ed7932a828c3810ffd011e85fb7696ff6a8365
rozina-aamir <rozina.aamir@outlook.com>
2026-02-12 09:09:58 +0000

    Add window prompt

--- rps.js
+++ rps.js
@@ -26,7 +26,7 @@ function getHumanChoice() {
     const log = document.querySelector("#log");
 
     playButton.addEventListener("click", () => {
-        let play = prompt("Rock, paper, or scissors.");
+        let play = window.prompt("Rock, paper, or scissors.");
 
         if (play = "rock") {
             log.innerText = "You won!";
