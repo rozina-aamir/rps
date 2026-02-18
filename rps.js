41e7d28b6138a9c068b270b1207a0e8d6aec5691
rozina-aamir <rozina.aamir@outlook.com>
2026-02-18 09:01:13 +0000

    Remove all code from getHumanChoice

--- rps.js
+++ rps.js
@@ -23,30 +23,8 @@ console.log(choice);
 
 // Log the prompt to the console
 
-function getHumanChoice(_play) {
-    const playButton = document.querySelector("#playButton");
-    const log = document.querySelector("#log");
-    console.log(playButton);
+function getHumanChoice() {
 
-        function play() {
-            playButton.addEventListener("click", () => {
-                console.log("Button clicked");
-                const humanChoice = prompt("Rock, paper, or scissors?");
-                console.log(humanChoice);
-            //
-            //    var play = window.prompt("Rock, paper, or scissors.");
-
-            //    if (play = "rock") {
-            //        log.innerText = "You won!";
-            //    } else if (play = "paper") {
-            //        log.innerText = "It's a draw.";
-            //    } else if (play = "scissors") {
-            //        log.innerText = "You lost.";
-            //    } else {
-            //    log.innerText = "Try again";
-            //    }
-            });
-        }
 }
 
 getHumanChoice();
