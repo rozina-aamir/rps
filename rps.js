16205ff8abaa83dd4dfdd8a22f5dda0093c98be8
rozina-aamir <rozina.aamir@outlook.com>
2026-02-19 09:14:37 +0000

    Move conditionals into function

--- rps.js
+++ rps.js
@@ -13,19 +13,19 @@ console.log(choice);
 
 function getHumanChoice() {
     return prompt("Rock, paper, scissors.");
+
+    if (humanChoice === "rock") {
+        return "You win!";
+    } else if (humanChoice === "paper") {
+        return "It is a draw.";
+    } else if (humanChoice === "scissors") {
+        return "You lost!";
+    } else {
+        return "Please try again";
+    }
 }
 
 playButton.addEventListener("click", () => {
     const humanChoice = getHumanChoice();
     console.log(humanChoice);
 });
-
-if (humanChoice === "rock") {
-    return "You win!";
-} else if (humanChoice === "paper") {
-    return "It is a draw.";
-} else if (humanChoice === "scissors") {
-    return "You lost!";
-} else {
-    return "Please try again";
-}
