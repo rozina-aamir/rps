19428106930d2f7df9245acf2919aa94fb146f3b
rozina-aamir <rozina.aamir@outlook.com>
2026-02-23 09:01:54 +0000

    Call playRound

--- rps.js
+++ rps.js
@@ -42,3 +42,5 @@ function playRound(humanChoice, computerChoice) {
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
